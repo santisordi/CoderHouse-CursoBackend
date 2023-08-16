27d7bb0f187870e716e105c1cec202d43bc3dd58
Santiago <santiagosordi@gmail.com>
2023-08-16 16:28:24 -0300

    se agregarn metodos

--- 1raPreEntregaSordi/src/controllers/ProductManager.js
+++ 1raPreEntregaSordi/src/controllers/ProductManager.js
@@ -1,19 +1,40 @@
 import { promises as fs} from 'fs';
+import {nanoid} from 'nanoid';
 
 class ProductManager{
     constructor (){
         this.path = "src/models/products.json";
     }
 
-    writeProducts = async (product) => {
+    readProducts = async ()=>{
         let products = await fs.readFile(this.path, "utf-8")
-        let productsParse = JSON.parse(products);
-        console.log(productsParse);
-        let productAll = [...productsParse, product];
-        await fs.writeFile(this.path, JSON.stringify(productAll));
+        return JSON.parse(products);
+    }
+
+    writeProducts = async (product) => {
+        await fs.writeFile(this.path, JSON.stringify(product));
+
+    }
+
+    addProducts = async (product)=>{
+        let productsOld = await this.readProducts();
+        product.id = nanoid();
+        let productAll = [...productsOld, product];
+        await this.writeProducts(productAll);
         return "Producto agregado correctamente";
 
     }
+
+    getProducts = async ()=>{
+        return await this.readProducts();
+    }
+
+    getProductsById = async (id)=>{
+        let products=  await this.readProducts();
+        let productById = products.find(prod => prod.id === id);
+        return productById;
+    }
+
 }
 
 export default ProductManager;
--- 1raPreEntregaSordi/src/index.js
+++ 1raPreEntregaSordi/src/index.js
@@ -3,18 +3,30 @@ import ProductManager from './controllers/ProductManager.js';
 
 const product = new ProductManager;
 
-const app = express();
 const PORT = 4000;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get("/products", async (req, res)=>{
+    res.send(await product.getProducts());  
+    
+}); 
+
+app.get("/products:id", async (req, res)=>{
+    let id = req.params.id;
+    res.send(await product.getProducts());  
+    
+}); 
+
+
 app.post("/products", async (req, res)=>{
     let newProduct = req.body;
-    response.send(await product.writeProducts(newProduct))       
-});
+    res.send(await product.addProducts(newProduct));       
+}); 
 
 app.listen(PORT, ()=>{
     console.log(`Servidor Express Puerto ${PORT}`);
-    console.log(`localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
