c1923479af64e1d2e19d987f9b8804d69a80c8e8
Santiago Sordi <santiagosordi@MacBook-Air-de-Santiago.local>
2023-10-20 07:26:02 -0300

    se corrigen authoritations

--- 8toDesafio Reestructura de nuestro servidor/src/router/product.routes.js
+++ 8toDesafio Reestructura de nuestro servidor/src/router/product.routes.js
@@ -7,11 +7,11 @@ productRouter.get('/', productsController.getProduct);
 //Ruta para traer un producto según su ID
 productRouter.get('/:pid', productsController.getProduct);
 //Ruta para crear un producto
-productRouter.post('/', productsController.postProduct);
+productRouter.post('/', passportError('jwt'), authorization('Admin'), productsController.postProduct);
 //Ruta para crear un producto o actualizar en caso de que exista
-productRouter.put('/:pid', productsController.putProduct);
+productRouter.put('/:pid', passportError('jwt'), authorization('Admin'), productsController.putProduct);
 //Ruta para borrar un producto según su ID
-productRouter.delete('/:pid', productsController.deleteProduct);
+productRouter.delete('/:pid', passportError('jwt'), authorization('Admin'), productsController.deleteProduct);
 
 export default productRouter;
 
