d18d445bc2e8cf6c2aebe3d9a5b05ea5a450e060
Santiago <santiagosordi@gmail.com>
2023-11-22 07:56:23 -0300

    implementacion logger

--- 10moDesafio Implementación de logger/backend/src/controllers/logger.controller.js
+++ 10moDesafio Implementación de logger/backend/src/controllers/logger.controller.js
@@ -2,6 +2,12 @@ import { logger } from "../utils/logger.js";
 
 export const getLog = (req, res) => {
     logger.info('mi primer log')
+    try {
+        throw new Error ('test error');
+    } catch (error) {
+        logger.info(`Ha ocurrido un error ${error.message}`);
+        res.status(500).send({message: 'Internal server error'});
+    }
     res.status(200).send('GET request to the main');
 };
 
--- 10moDesafio Implementación de logger/backend/src/router/product.routes.js
+++ 10moDesafio Implementación de logger/backend/src/router/product.routes.js
@@ -16,19 +16,20 @@ productRouter.get('/:pid', productsController.getProduct);
 //Mocking products
 productRouter.get('/mockingproducts', passportError('jwt'), authorization('Admin'), generateMockProducts)
 //Ruta para crear un producto
-sessionRouter.post('/register', (req, res, next) => {
-    const { first_name, last_name, email, password, age } = req.body;
-    if (!last_name || !first_name || !email || !password || !age) {
-       const customError = CustomError.createError({
-            name: 'Error creating user',
-            cause: generateUserErrorInfo({ first_name, last_name, email, password, age }),
-            message: 'All fields must be completed',
-            code: EErrors.DATOS_INVALIDOS_ERROR
-        });
-        return next(customError);
-    }
-    next();
-  }, passport.authenticate('register'), sessionController.registerPost);
+productRouter.post('/', (req,res,next) => {
+    const { title, description, stock, price, code, category } = req.body;
+  
+        if ((!title || !description || !stock || !price || !code || !category)) {
+            const customError = CustomError.createError({
+                name: 'Error creating product',
+                cause: generateProductErrorInfo({ title, description, stock, price, code, category }),
+                message: 'All fields must be completed',
+                code: EErrors.ROUTING_ERROR,
+            });
+            return next(customError);
+        };
+        next();
+}, passportError('jwt'), authorization('Admin'), productsController.postProduct);
 //Ruta para crear un producto o actualizar en caso de que    exista
 productRouter.put('/:pid', passportError('jwt'), authorization('Admin'), productsController.putProduct);
 //Ruta para borrar un producto según su ID
