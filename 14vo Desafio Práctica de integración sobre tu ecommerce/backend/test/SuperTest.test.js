49e5cf169ea0776e05fdf06d19039da180850f95
Santiago <santiagosordi@gmail.com>
2023-12-27 15:16:41 +0100

    supertestusers1

--- 14vo Desafio Práctica de integración sobre tu ecommerce/backend/test/SuperTest.test.js
+++ 14vo Desafio Práctica de integración sobre tu ecommerce/backend/test/SuperTest.test.js
@@ -1,115 +1,166 @@
-import mongoose from "mongoose";
+import 'dotenv/config';
 import chai from 'chai';
-import supertest from  'supertest';
-import 'dotenv/config.js';
+import supertest from 'supertest';
+import mongoose from 'mongoose';
+
 import { logger } from "../src/utils/logger.js";
+import productsModel from "../src/models/products.model.js";
+import cartModel from '../src/models/carts.models.js';
+import { userModel } from '../src/models/users.model.js';
 
-before(async () => {
-    this.timeout(5000);
-    await mongoose
-        .connect(process.env.MONGO_URL)
-        .then(() => {
-            logger.info("MongoDB connected");
-        })
-        .catch((error) => logger.info(`Error connecting to MongoDB Atlas: ${error}`));
-});
+await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(async () => {
+        logger.info("MongoDB connected");
+    })
+    .catch((error) => console.log(`Error connecting to MongoDB Atlas: ${error}`));
 
-after(async () => {
-    await mongoose.disconnect();
-});
-const requester = supertest('http://localhost:4000');
-const {expect} = chai;
+const expect = chai.expect
+const requester = supertest('http://localhost:3000')
 
-describe('Test de Usuario y carrito', async () => { 
-    // before(async()=> { //el before se utiliza antes que todo para llamar a los modelos
-    //     //conectarme al usuario
-    //     //conectarme a product
-    // });
-    // //por cada ejecucion de mi proyecto hago lo siguiente con el beforeEach
-    // beforeEach(async()=>{
-    //     //generar nuevo usuario
-    //     //generar nuevo producto
-    // });
-
-    let token = {};
-    let cartId = "";
-    let userId = "";
-    let newUser = {
+describe('App tests', () => {
+    let token = {}
+    let cartId = ''
+    let userId = ''
+    let productId = ''
+    const newUser = {
         first_name: 'User',
         last_name: 'Test',
         email: 'user@test.com',
+        age: 1234,
         password: '1234'
     };
 
-    //esta ok
-    it("Ruta: api/sessions/register para crear usuario"), async ()=>{
- 
-       const { __body, status } = await requester.post('api/sessions/register').send(newUser);
-        expect(status).to.be.equal(200);
-        logger.info( `Status: ${__body}`);
-    };
+    it('Endpoint test /api/sessions/register, a new user is expected to be created', async function () {
+        this.timeout(7000)
 
-    //esta ok
-    it('Ruta: api/session/login con el metodo POST', async()=>{
-        const response = await requester.post('api/sessions/lgin').send(newUser);
-        const {__body} = response
-        const tokenResult = response.header['jwt-cookie'][0]; 
+        const { status } = await requester
+        .post('/api/sessions/register')
+        .send(newUser)
+        const user = await userModel.findOne({ email: newUser.email });
 
-        expect(tokenResult).to.be.ok; // comprueba la existencia del elemento
-    
-        expect(response.status).to.be.equal(200) ;// consulto si la respuesta de la peticion es = a 200
-    
+        if (status === 302) {
+            //User created succesfully
+            expect(status).to.equal(302) // Código 302 porque al registrarse hace una redirección a la vista de inicio de sesión
+            logger.info(`User created succesfully. Status: ${status}`)
+            logger.info(`User: ${user}`)
+        } else if (status === 401) {
+            //User already exists
+            expect(status).to.equal(401)
+            logger.info(`User already exists. Status: ${status}`)
+        } else {
+            //Any other unexpected status
+            throw new Error(`Unexpected status code: ${status}`)
+        }
+    });
+
+    it('Endpoint test /api/sessions/login, a user is expected to log in', async function () {
+        this.timeout(7000)
+
+        const { status, header} = await requester
+        .post('/api/sessions/login')
+        .send(newUser);
+        const tokenResult = header['set-cookie'][0];
+
+        expect(tokenResult).to.be.ok;
+        expect(status).to.be.equal(302);//status(302) de nuevo por ser redirect
+        logger.info(`Login status: ${status}`);
         token = {
-            name: tokenResult.split("=")[0],
-            value: tokenResult.split("=")[1]
+            name: tokenResult.split('=')[0],
+            value: tokenResult.split('=')[1]
         };
 
-        expect( token.value ).to.be.ok;
-        expect( token.name ).to,be.ok.and.equal('jwt-Cookie');
-        expect( __body.cartId ).to.be.ok;
+        expect(token).to.be.ok;
+        expect(token.name).to.be.ok.and.equal('jwtCookie');
 
-        userId = __body._id;
-        cartId = __body.cartId; 
-        console.log(`Token:  ${token.name} = ${token.value}`);
+        const user = await userModel.findOne({ email: newUser.email })
+        logger.info(user)
+        userId = user._id
+        cartId = user.cart._id
+        logger.info(`Token: ${token.name} = ${token.value}`); 
     });
 
-    //agregar un producto al carrito
-    it('Ruta: api/carts.product/p:id con metodo POST', async()=>{
-        const cid = cartId;
-        const pid = "" //modelo de producto o prodcto de prueba
-
-        // await requester.post(`/api/carts/products/${pid}`).set('Cookie', [`${token.name} = ${token.value}`]);
-
-        const { __body, status } = await requester.post(`/api/carts/${cid}/product/${pid}`).set('Cookie', [`${token.name} = ${token.value}`]);
-
-        expect(status).to.be.equal(200);       
-        logger.info("Agregado producto en api carts");
-    });
-    //actualizar la cantidad
-    it('Ruta: api/carts/cid/product/.pid metodo PUT', async()=>{
-
-        const cid = cartId;
-        const pid = "" //modelo de producto
-        const newQuantity = { quantity: 6};
-
-        //hago la consulta y envio con la cookie la cantiadad
-        const { __body, status }  = await requester.put(`/api/carts/${cid}/product/${pid}`).send(newQuantity).set('Cookie', [`${token.name} = ${token.value}`]);
-        //cmpruebo si es correcto
-        expect(status).to.be.equal(200);
-        logger.info("Cantidad producto actualizada en api carts");
-        logger.info(`Status: ${__body}`);
+    it('Endpoint test /api/sessions/current, it is expected to get the current user', async function () {
+        const { statusCode, ok } = await requester
+            .get('/api/sessions/current')
+            .set('Cookie', [`jwtCookie=${token.value}`]);
+        logger.info(statusCode, ok);
+        logger.info(`Token: ${token.name} = ${token.value}`);
     });
 
-    //eliminar el usuario
-    it('Ruta: api/users/uid metodo DELETE', async()=>{
-        const uid = userId;
-        const { __body, status } = await requester.delete(`/api/users/${uid}`).set('Cookie', [`${token.name} = ${token.value}`]);
+    it('Endpoint test /api/carts/:cid/product/:pid, a product is expected to be added to the cart', async function () {
+        const cid = cartId
+        const newProduct = await productsModel.create({
+            title:"test product",
+            description:"this is a test product",
+            price:100,
+            stock:500,
+            category:"testing",
+            code:`test${Math.random().toString(36).substring(7)}`
+        })
+        productId = newProduct._id
+        const quantity = 1 
+
+        /* await requester.post(`/api/carts/${cid}/product/${productId}`).set('Cookie', [`${token.name} = ${token.value}`]) TO ADD TWICE THE SAME PRODUCT */ 
+        const { status } = await requester
+        .put(`/api/carts/${cid}/product/${productId}`)
+        .send({ quantity })
+        .set('Cookie', [`${token.name} = ${token.value}`])
+
+        logger.info(`Status: ${status}`);
+
+        expect(status).to.equal(200)
+
+        logger.info('Product added succesfully')
+        const productData = newProduct.toJSON()
+        logger.info(`Product: `, productData)
+    })
+
+    it('Endpoint test /api/carts/:cid/products/:pid, it is expected to modify the quantity of a product in the cart', async function () {
+        const cid = cartId
+        const newQty = { quantity : 20 }
         
-        expect(status).to.be.equal(200);
-        logger.info("Usuario eliminado en api/user");
-        logger.info(`Status: ${__body}`); 
-    });
+        const { status } = await requester
+        .put(`/api/carts/${cid}/products/${productId}`)
+        .send(newQty)
+        .set('Cookie', [`${token.name} = ${token.value}`])
+
+        expect(status).to.equal(200)
+
+        const updatedCart = await cartModel.findById(cid)
+        const updatedProduct = updatedCart.products.find(product => product.id_prod.toString() === productId.toString())
+        if(updatedProduct){
+            logger.info(`Product qty successfully updated, new qty: ${updatedProduct.quantity}`)
+        }else {
+            logger.error('Product not found in the updated cart.');
+        }
+        logger.info(`Status: ${status}`)
+    })
+
+    it('Endpoint test /api/users/deleteOne/:id, it is expected to delete an user', async function () {
+
+        const { status } = await requester
+        .delete(`/api/users/deleteOne/${userId}`)
+        .set('Cookie', [`${token.name} = ${token.value}`])
+
+        if (status === 200){
+            await deleteCart(userId)
+        }
+
+        expect(status).to.equal(200)
+        logger.info('User deleted succesfully')
+        logger.info(`Status: ${status}`)
+
+        async function deleteCart(userId) {
+            const user = await userModel.findById(userId)
+            if (user) {
+                const cartId = user.cart
+    
+                if (cartId) {
+                    await cartModel.findByIdAndDelete(cartId)
+                    logger.info('Cart deleted successfully')
+                }
+            }
+        }
+    })
 });
-
-
-
--- 14vo Desafio Práctica de integración sobre tu ecommerce/backend/test/products/products.test.js
+++ 14vo Desafio Práctica de integración sobre tu ecommerce/backend/test/products/products.test.js
@@ -1,40 +1,8 @@
-import chai from "chai";
-import supertest from "supertest";
 import Assert from 'assert'
 import mongoose from "mongoose";
 import 'dotenv/config';
 import { logger } from "../../src/utils/logger.js";
 import productsModel from "../../src/models/products.model.js";
-
-
-// const expect = chai.expect;
-// const requester = supertest('http://localhost:3000');
-
-// await mongoose.connect(process.env.MONGO_URL)
-//     .then(() => logger.info('Conectado a la BDD Mongo (test mode)'))
-//     .catch(error => logger.error('Error al conectar a la BDD Mongo (test mode): ' + error));
-
-
-// let idProd;
-
-// describe ('Testing App', ()=>{
-//     describe('Test de productos', ()=>{
-//         it('Test endpoint /ap/products, se espera ue genere un producto', async function(){
-//             const newProduct = {    
-//                 title: 'mate 1',
-//                 description: 'mate calabaza',
-//                 category: 'Calabaza',
-//                 price: 1500,
-//                 stock: 60,
-//                 code: 'Mate01',
-//                 thumbnail: []
-//             }
-//             const {_body } = await requester.post('/api/products').send(newProduct);
-//             logger.info(_body);
-//             // idProd = _body.payload._id 
-//         });
-//     });
-// });
 
 await mongoose
     .connect(process.env.MONGO_URL)
