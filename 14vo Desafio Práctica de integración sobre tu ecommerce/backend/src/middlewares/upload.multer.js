4ec664334434f40e6ef603b80cd8a214e170335d
Santiago <santiagosordi@gmail.com>
2023-12-29 00:38:59 +0100

    ultimo desafio multer v2

--- 14vo Desafio Práctica de integración sobre tu ecommerce/backend/src/middlewares/upload.multer.js
+++ 14vo Desafio Práctica de integración sobre tu ecommerce/backend/src/middlewares/upload.multer.js
@@ -5,9 +5,9 @@ const storage = multer.diskStorage({
     cb(null, 'uploads');
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix);
-  }
+    console.log(file)
+    cb(null, `${Date.now()} - ${file.originalname}`);
+  } 
 });
 
 const upload = multer({ storage: storage });
